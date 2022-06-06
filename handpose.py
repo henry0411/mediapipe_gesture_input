16bc8daa2df7777fb01cd49c2ce5fc978a357b68
henry0411 <0411henry@gmail.com>
2022-06-07 01:56:50 +0800

    顯示手部動作，影像上方會顯示手指比出的數字

---
+++ handpose.py
@@ -0,0 +1,41 @@
+# 手勢名稱
+def hand_pos(finger_angle):
+    f1 = finger_angle[0]   # 大拇指角度
+    f2 = finger_angle[1]   # 食指角度
+    f3 = finger_angle[2]   # 中指角度
+    f4 = finger_angle[3]   # 無名指角度
+    f5 = finger_angle[4]   # 小拇指角度
+
+    # 小於 50 表示手指伸直，大於等於 50 表示手指捲縮
+    if f1<50 and f2>=50 and f3>=50 and f4>=50 and f5>=50:
+        return 'good'
+    elif f1>=50 and f2>=50 and f3<50 and f4>=50 and f5>=50:
+        return 'no!!!'
+    elif f1<50 and f2<50 and f3>=50 and f4>=50 and f5<50:
+        return 'ROCK!'
+    elif f1>=50 and f2>=50 and f3>=50 and f4>=50 and f5>=50:
+        return '0'
+    elif f1>=50 and f2<50 and f3>=50 and f4>=50 and f5>=50:
+        return '1'
+    elif f1>=50 and f2<50 and f3<50 and f4>=50 and f5>=50:
+        return '2'
+    elif f1>=50 and f2>=50 and f3<50 and f4<50 and f5<50:
+        return 'ok'
+    elif f1<50 and f2>=50 and f3<50 and f4<50 and f5<50:
+        return 'ok'
+    elif f1>=50 and f2<50 and f3<50 and f4<50 and f5>50:
+        return '3'
+    elif f1>=50 and f2<50 and f3<50 and f4<50 and f5<50:
+        return '4'
+    elif f1<50 and f2<50 and f3<50 and f4<50 and f5<50:
+        return '5'
+    elif f1<50 and f2>=50 and f3>=50 and f4>=50 and f5<50:
+        return '6'
+    elif f1<50 and f2<50 and f3>=50 and f4>=50 and f5>=50:
+        return '7'
+    elif f1<50 and f2<50 and f3<50 and f4>=50 and f5>=50:
+        return '8'
+    elif f1<50 and f2<50 and f3<50 and f4<50 and f5>=50:
+        return '9'
+    else:
+        return ''
